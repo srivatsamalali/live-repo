b19c2c58c653bf026b232f738d66df8c523dea52
CC2003User <crafted.coded.2003@gmail.com>
2020-10-16 10:14:14 +0530

    .

---
+++ java/SingleInheritance.java
@@ -0,0 +1,43 @@
+public class SingleInheritance {
+	public static void main(String[] args) {
+      // A obj = new A();
+      // obj.disp();
+      // A obj1 = new A("Today is a great day!");
+      // obj1.disp();
+
+      B bobj = new B();
+      //bobj.disp();
+      bobj.disp();
+      //bobj.super.disp();
+   }
+}
+
+class A {
+   public String message = "welcome";
+	public A() { 
+		System.out.println("A()");
+	}
+	public A(String pmessage) {
+		System.out.println("A(String message)");
+		message = pmessage;
+	}
+
+	public void disp() {
+      System.out.println("base disp()");
+      //System.out.println(message);
+		//System.out.println("base or parent or super");
+	}
+}
+
+class B extends A{
+   public B(){
+   super();
+   System.out.println("B()");
+   }
+   //overriding is the pricinple of the derived method supressing the base method.
+	public void disp() {
+       super.disp();
+       System.out.println("dervied disp()");
+		//System.out.println("base or parent or super");
+	}
+}
