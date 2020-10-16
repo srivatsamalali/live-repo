25be03b8e6bfb8fcfc2a12d40fdfb40c3167d906
CC2003User <crafted.coded.2003@gmail.com>
2020-10-16 12:30:49 +0530

    .

--- java/oops/Materials.java
+++ java/oops/Materials.java
@@ -2,21 +2,29 @@
 public class Materials {
 	public static void main(String[] args) {
    //Make Door and set price
+   Door door = new Door();
+   door.price = 500;
+   Window window = new Window();
+   window.price = 600;
+
+   Materials materials = new Materials();
+   int price = materials.addMaterial(door,window);
+   System.out.println(price);
    //Make Window and set price
    //use all 4 addMaterial
    }
 
    public int addMaterial(int doorPrice, int windowPrice){
-//
+      return 0;
    }
       public int addMaterial(Door door, Window window){
-//
+         return door.price + window.price;
    }
       public int addMaterial(int doorPrice,  Window window){
-//
+      return 0;
    }
       public int addMaterial(Door door, int windowPrice){
-//
+      return 0;
    }
 }
 
