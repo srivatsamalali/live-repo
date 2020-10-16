8f1c2bc24abc67b9dd3b670206771c3cffa7db29
CC2003User <crafted.coded.2003@gmail.com>
2020-10-16 09:34:47 +0530

    .

--- java/ConstructorExample.java
+++ java/ConstructorExample.java
@@ -4,6 +4,11 @@ public class ConstructorExample{
       Mobile samsungMobile;
       //create an instance and assign it to the reference
       samsungMobile = new Mobile();
+      //call the method
+      samsungMobile.display();
+
+      Mobile samsungMobile1 = new Mobile("Nokia", 567f);
+      samsungMobile1.display();
    }
 }
 
@@ -14,9 +19,19 @@ class Mobile{
    //Constructors are functions without a return type
    //same name of the class
    // purpose is to initialize variables
+   // default constructor does not contain parameter
    public Mobile(){
       System.out.println("Default Constructor");
       name = "Samsung";
       price = 30.50f;
+   }
+   public Mobile(String pName, float pPrice){
+      System.out.println("Parameterized Constructor");
+      name =pName;
+      price = pPrice;
+   }
+   public void display(){
+      System.out.println(name);
+      System.out.println(price);
    }
 }
