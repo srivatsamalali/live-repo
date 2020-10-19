b48ce2eb750bdca3c0857224eb77cf6dbfc40e2f
CC2003User <crafted.coded.2003@gmail.com>
2020-10-19 13:23:26 +0530

    .

---
+++ java/collections/TreeSetEx.java
@@ -0,0 +1,32 @@
+import java.util.TreeSet;
+import java.util.Collections;
+public class TreeSetEx{
+public static void main(String[] args) {
+   start();
+}
+   public static void start(){
+      TreeSet age = new TreeSet();
+      age.add(5);
+      age.add(6);
+      age.add(2);
+      age.add(1);
+      System.out.println(age);
+   }
+}
+
+// allows nulls
+// allows duplicates
+// insertion order
+// sysout(age) ? 
+// age.add(5);
+// age.add(5.5f);
+// change the 3rd item to Akshay?
+// remove the 4 th item which is null.
+// print the size of the TreeSet.
+// display the collection in sorted order
+// remove the second element
+// iterate and display the elements
+// remove all the elements
+
+
+
