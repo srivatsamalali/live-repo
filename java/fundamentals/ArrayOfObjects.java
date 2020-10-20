83d0586c67c7d664ea79abe6e7f5c80866f3d844
CC2003User <crafted.coded.2003@gmail.com>
2020-10-20 10:46:11 +0530

    .

---
+++ java/fundamentals/ArrayOfObjects.java
@@ -0,0 +1,28 @@
+package fundamentals;
+
+public class ArrayOfObjects
+{
+	public static void main(String[] args) {
+		//Create a referene
+		Product product = null;
+		//create instance
+		product = new Product();
+		//access the member variables
+		System.out.println(product.productNameString);
+		System.out.println(product.descriptoinString);
+		System.out.println(product.price);
+		//access the member functions
+		product.displayDetails();
+	}
+}
+ class Product {
+	public String productNameString;
+	public String descriptoinString;
+	public float price;
+
+	public void displayDetails() {
+		System.out.println(productNameString);
+		System.out.println(descriptoinString);
+		System.out.println(price);
+	}	
+}
